d34705475fedce6728acaa619c9ecf4f8f24a0e1
Chantz-J <johnson.chantz@gmail.com>
2021-01-26 18:00:00 -0500

    Finished task 3

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(data, cb) {
+    const years = cb(data).map(x => x.Year)
+    return years
 }
 
 
@@ -61,8 +62,9 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-    /* code here */
+function getWinners(data, cb) {
+    const winners = cb(data).map(x => x['Home Team Goals'] >= x['Away Team Goals'] ? x['Home Team Name'] : null)
+    return winners
 }
 
 
